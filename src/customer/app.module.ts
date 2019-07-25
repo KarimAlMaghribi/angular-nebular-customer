fe80aaf618387dba9cdafece06ec7efd7ac3a91e
KarimAlMaghribi <student.abdelkarim@gmail.com>
2019-07-26 01:19:57 +0200

    2 files modified

--- src/customer/app.module.ts
+++ src/customer/app.module.ts
@@ -5,8 +5,10 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { NbThemeModule } from '@nebular/theme';
+
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, NbThemeModule.forRoot(), ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
