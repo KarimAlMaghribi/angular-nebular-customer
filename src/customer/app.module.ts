8b2cd7a3a1811687d821601525aa9daabaa11e02
KarimAlMaghribi <student.abdelkarim@gmail.com>
2019-07-26 01:52:06 +0200

    2 files modified, 3 added

--- src/customer/app.module.ts
+++ src/customer/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 import { NbThemeModule } from '@nebular/theme';
+import { CustomerspacePagecomponentComponent } from './customerspace/customerspace.pages/customerspace-pagecomponent/customerspace-pagecomponent.component';
+import { CustomerspacePageComponent } from './customerspace/customerspace.pages/customerspace-page/customerspace-page.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, NbThemeModule.forRoot(), ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, NbThemeModule.forRoot() ],
+  declarations: [ AppComponent, HelloComponent, CustomerspacePagecomponentComponent, CustomerspacePageComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/customer/customerspace/customerspace-page/customerspace-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customerspace-page',
+  templateUrl: './customerspace-page.component.html',
+  styleUrls: ['./customerspace-page.component.css']
+})
+export class CustomerspacePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
