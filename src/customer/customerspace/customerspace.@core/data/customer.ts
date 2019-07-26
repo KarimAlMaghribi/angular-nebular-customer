07b6d288291352bad9b386fcc9971af8aed43f99
KarimAlMaghribi <student.abdelkarim@gmail.com>
2019-07-26 02:24:37 +0200

    1 file modified

--- src/customer/customerspace/customerspace.@core/data/customer.ts
+++ src/customer/customerspace/customerspace.@core/data/customer.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 
 export interface Customer {
+  id: number;
   name: string;
   lastname: string;
   phonenumber: string;
@@ -9,8 +10,22 @@ export interface Customer {
 
 export abstract class Customer {
 
+//getter
   abstract getAllCustomer(): Observable<Customer[]>;
   abstract getCustomerById(id: number): Observable<Customer>;
   abstract getCustomerByPhoneNumber(id: number): Observable<Customer>;
+
+//setter  
+  abstract setLocalCustomer(customer: Customer);
+  abstract setSystemCustomer(customer: Customer);
+
+//edit customer instance
+  abstract setCustomerName(id: number, name:string);
+  abstract setCustomerLastName(id: number, lastname:string);
+  abstract setCustomerPhoneNumber(id: number, phoneNumber:string);
+
+
+  
+
 
 }
